7df732fd61c386dfb9b3cfd2a09be94349357c84
Venco <v3nco@v3nco.dev>
2025-12-27 22:25:19 +0100

    awawawawaw

---
+++ src/routes/blahaj/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+    const urlParams = new URLSearchParams(window.location.search);
+    const hasId = urlParams.has('id');
+</script>
+
+(#if hasId)
+    <h1></h1>
+(:else)
+    <h1>uhm this is missing an ID so what am i supposed to do? I don't have any profile to show you...</h1>
+(/if)
