1cddcc0a5d31d7f1c6b5166a56a3485cfa80d24a
Venco <v3nco@v3nco.dev>
2025-12-28 01:04:36 +0100

    and actually commit the schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,13 +1,17 @@
-import { pgTable, serial, integer, varchar, date, json, text } from 'drizzle-orm/pg-core';
+import { pgTable, integer, varchar, date, json, text, real } from 'drizzle-orm/pg-core';
 
-export const user = pgTable('user', { id: serial('id').primaryKey(), age: integer('age') });
-
-export const blahajsTable = pgTable('users', {
+export const blahajsTable = pgTable('blahajs', {
 	id: integer().primaryKey().generatedAlwaysAsIdentity(), // the id
 	name: varchar({ length: 255 }).notNull(), // the name of the haj
 	date: date(), // the date the blahaj was adopted
 	size: varchar({ length: 255 }).notNull(), // the size of the haj
 	modifications: json(), // if it was modified in any way and how/what
 	location: json(), // where the blahaj was adopted
-	description: text()
+	description: text(),
+	pronouns: varchar({ length: 255 }).notNull(), // the blahaj's pronouns!
+	gender: varchar({ length: 255 }).notNull(), // the blahaj's gender!
+	floof: integer(), // level of floofiness on a scale of 1-10 (stars with halves)
+	squish: integer(), // The level of squishiness on a scale of 1-10 (stars with halves)
+	lastwashed: date(), // last time the haj has been washed
+	mloftearsabsorbed: real() //i want to make this an easter egg
 });
