dcd2c3b27d06dc023aad8f61d94679a902c450d3
Venco <v3nco@v3nco.dev>
2025-12-28 01:04:11 +0100

    Suya:tm: fields! (and fixing my database mess)

---
+++ drizzle/0001_lumpy_peter_parker.sql
@@ -0,0 +1,6 @@
+ALTER TABLE "blahajs" ADD COLUMN "pronouns" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "blahajs" ADD COLUMN "gender" varchar(255) NOT NULL;--> statement-breakpoint
+ALTER TABLE "blahajs" ADD COLUMN "floof" integer;--> statement-breakpoint
+ALTER TABLE "blahajs" ADD COLUMN "squish" integer;--> statement-breakpoint
+ALTER TABLE "blahajs" ADD COLUMN "lastwashed" date;--> statement-breakpoint
+ALTER TABLE "blahajs" ADD COLUMN "mloftearsabsorbed" real;
