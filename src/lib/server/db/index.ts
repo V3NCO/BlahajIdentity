942915044a86ec85da5e63ea33adac06d518bc7c
Venco <v3nco@v3nco.dev>
2025-12-29 15:10:34 +0100

    Database thing when docker build

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -3,8 +3,6 @@ import postgres from 'postgres';
 import * as schema from './schema';
 import { env } from '$env/dynamic/private';
 
-if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-
-const client = postgres(env.DATABASE_URL);
-
+const connectionString = env.DATABASE_URL || 'postgres://localhost:5432/blahajid';
+const client = postgres(connectionString);
 export const db = drizzle(client, { schema });
